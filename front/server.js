a4573df31532ea33e905cfbbfca292bb81edc0c5
Phillip <santilanphillip23@gmail.com>
2024-01-22 22:56:51 +0800

    first commit

---
+++ front/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+//endpoint starts here
+//req -> request
+//res -> response
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+
+app.get('/add', (req, res)=>{
+    res.sendFile(__dirname + '/public/add.html');
+})
+
+app.get('/update/:profileId', (req, res)=>{
+    res.sendFile(__dirname + '/public/update.html');
+})
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + '/public/home.html');
+})
+
+
+app.get('/delete/:profileId', (req, res)=>{
+    res.sendFile(__dirname + '/public/delete.html');
+})
+
+
+
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname + '/public/about-us.html');
+})
+
+app.get('/contact', (req, res)=>{
+    res.sendFile(__dirname + '/public/contact-us.html');
+})
+
+app.get('/login', (req, res)=>{
+    res.sendFile(__dirname + '/public/login.html');
+})
+
+app.get('/logout', (req, res)=>{
+    res.sendFile(__dirname + '/public/logout.html');
+})
+
+
+
+
+
+app.listen(port);
+console.log('front end server is now running in port '+port);
